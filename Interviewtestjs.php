f1033e283aaf1410635d91a769c9ddc7259c5708
hunter961103 <37042097+hunter961103@users.noreply.github.com>
2020-08-04 18:03:08 +0800

    Add files via upload

---
+++ Interviewtestjs.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Interview Test</title>
+</head>
+<!--Sorting user input ordered by uppercase letter, lowercase letter, number and others without refreshing page-->
+<script>
+    function sortInput() {
+        var input = document.getElementById("input").value;
+        var characters = input.split("");
+        var uppercase = "", lowercase = "", number = "", others = "";
+        for(var i = 0; i < characters.length; i++) {
+            if(characters[i] === characters[i].toUpperCase() && characters[i] !== characters[i].toLowerCase()) {
+                uppercase = uppercase + characters[i];
+            }
+            else if(characters[i] === characters[i].toLowerCase() && characters[i] !== characters[i].toUpperCase()) {
+                lowercase = lowercase + characters[i];
+            }
+            else if(!isNaN(characters[i])) {
+                number = number + characters[i];
+            }
+            else {
+                others = others + characters[i];
+            }
+        }
+        var split_uppercase = uppercase.split("");
+        var sort_uppercase = split_uppercase.sort();
+        uppercase = sort_uppercase.join("");
+        var split_lowercase = lowercase.split("");
+        var sort_lowercase = split_lowercase.sort();
+        lowercase = sort_lowercase.join("");
+        var split_number = number.split("");
+        var sort_number = split_number.sort();
+        number = sort_number.join("");
+        document.getElementById("output").innerHTML = "Output: " + uppercase + lowercase + number + others;
+    }
+</script>
+<body>
+    <form method="post">
+	    <label>Input here: </label>
+        <input type="text" id="input">
+	    <input type="button" value="Submit "onclick="sortInput()">
+        <br>
+        <label id="output"></label>
+    </form>
+</body>
+</html>
---
+++ Interviewtestphp.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Interview Test</title>
+</head>
+<!--Sorting user input ordered by uppercase letter, lowercase letter, number and others with refreshing page-->
+<?php
+    $input = ""; $uppercase = ""; $lowercase = ""; $number = ""; $others = "";
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $input = $_POST["input"];
+    }
+    $characters = str_split($input);
+    for($i = 0; $i < sizeof($characters); $i++) {
+        if(ctype_upper($characters[$i])) {
+            $uppercase = $uppercase.$characters[$i];
+        }
+        else if(ctype_lower($characters[$i])) {
+            $lowercase = $lowercase.$characters[$i];
+        }
+        else if(is_numeric($characters[$i])) {
+            $number = $number.$characters[$i];
+        }
+        else {
+            $others = $others.$characters[$i];
+        }
+    }
+    $split_uppercase = str_split($uppercase);
+    sort($split_uppercase);
+    $uppercase = implode($split_uppercase);
+    $split_lowercase = str_split($lowercase);
+    sort($split_lowercase);
+    $lowercase = implode($split_lowercase);
+    $split_number = str_split($number);
+    sort($split_number);
+    $number = implode($split_number);
+    $result = $uppercase.$lowercase.$number.$others;
+?>
+<body>
+    <form method="post">
+	    <label>Input here: </label>
+        <input type="text" name="input">
+	    <input type="submit" value="Submit">
+        <br>
+        <label><?php if($result != "") {echo "Result: ".$result;} ?></label>
+    </form>
+</body>
+</html>
